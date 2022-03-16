570d99cecaa7475cad4a827f7bc1675d43aab6e7
unknown <irfansyed.pg@gmail.com>
2022-03-16 16:59:09 +0500

    all

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^h%j4sbkcp&qbs5m!oxc7)owvoi4*j5@k6)-)@t6rd3ofvg2ey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecomeactual.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -75,14 +75,23 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':'demo_1',
+#         'USER':'irfansyed',
+#         'PASSWORD':'12345678',
+#         'HOST':'database-1.ct57ovwdmjd0.us-east-1.rds.amazonaws.com',
+#         'PORT':'5432'
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'demo_1',
-        'USER':'irfansyed',
-        'PASSWORD':'12345678',
-        'HOST':'database-1.ct57ovwdmjd0.us-east-1.rds.amazonaws.com',
-        'PORT':'5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':os.path.join(BASE_DIR,'db.sqlite3'),
+     
     }
 }
 
