b2cb3131b8ad710ad62950f075ae30db8e84f53a
unknown <irfansyed.pg@gmail.com>
2022-06-14 02:42:06 +0500

    added whatever functionality on some model, refs #2

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/4.0/ref/settings/ check 
+https://docs.djangoproject.com/en/4.0/ref/settings/ check  ddd
 """
 
 from pathlib import Path
